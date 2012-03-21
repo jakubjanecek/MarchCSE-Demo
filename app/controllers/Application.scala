c9908f3d33245f1d6d00dae33766d7aaaa1e4609
Jakub Janecek <janecjak@fit.cvut.cz>
2012-03-21 12:05:24 +0100

    Last commit incorrect - now Twitter Bootstrap added. Main layout changed - simple layout for our demo. Three new routes and methods in Application controller created - not implemented yet.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,21 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Hello Scala Enthusiasts."))
   }
-  
+
+  def buy(id: Long) = Action {
+    NotImplemented
+  }
+
+  def addDrink = Action {
+    NotImplemented
+  }
+
+  def addDrinkSubmit = Action {
+    NotImplemented
+  }
+
 }
