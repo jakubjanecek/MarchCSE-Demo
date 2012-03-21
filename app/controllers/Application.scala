29a71d1978a1422f5727e1590b188c1369f3c07d
Jakub Janecek <janecjak@fit.cvut.cz>
2012-03-21 13:48:18 +0100

    All functionality finished. All model and controller methods implemented and views formatted.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,24 +1,56 @@
 package controllers
 
 import play.api._
+import data.Form
+import data.Forms._
 import play.api.mvc._
+import models.Drink
+import anorm.NotAssigned
 
 object Application extends Controller {
 
+  private val form = Form(
+    mapping(
+      "name" -> nonEmptyText(1, 100),
+      "price" -> number(max = 1000),
+      "kind" -> nonEmptyText(1, 20)
+    )
+        ((name, price, kind) => Drink(NotAssigned, name, price, kind))
+        ((drink: Drink) => Some(drink.name, drink.price, drink.kind))
+  )
+
   def index = Action {
-    Ok(views.html.index("Hello Scala Enthusiasts."))
+    implicit request =>
+      Ok(views.html.index(Drink.list, messages))
   }
 
   def buy(id: Long) = Action {
-    NotImplemented
+    Drink.find(id) match {
+      case Some(drink) => Redirect(routes.Application.index).flashing("success" -> "You bought a delicious %s for $%d.".format(drink.name, drink.price))
+      case None => Redirect(routes.Application.index).flashing("error" -> "Drink not found!")
+    }
   }
 
   def addDrink = Action {
-    NotImplemented
+    Ok(views.html.addDrink(form))
   }
 
   def addDrinkSubmit = Action {
-    NotImplemented
+    implicit request =>
+      val bindedForm = form.bindFromRequest
+      bindedForm.fold(
+        errorForm => BadRequest(views.html.addDrink(errorForm)),
+        drink => {
+          if (Drink.create(drink)) {
+            Redirect(routes.Application.index).flashing("success" -> "OK")
+          }
+          else {
+            Redirect(routes.Application.index).flashing("error" -> "ERROR")
+          }
+        }
+      )
   }
+
+  private def messages(implicit request: RequestHeader): Seq[String] = request.flash.data.map(_._2).toSeq
 
 }
--- app/models/Drink.scala
+++ app/models/Drink.scala
@@ -1,6 +1,9 @@
 package models
 
-import anorm.Pk
+import anorm._
+import anorm.SqlParser._
+import play.api.db.DB
+import play.api.Play.current
 
 case class Drink(id: Pk[Long],
                  name: String,
@@ -8,7 +11,53 @@ case class Drink(id: Pk[Long],
                  kind: String)
 
 object Drink {
-  
-  
+
+  private val aDrink = {
+    get[Pk[Long]]("id") ~
+        str("name") ~
+        int("price") ~
+        str("kind") map {
+      case id ~ name ~ price ~ kind => Drink(id, name, price, kind)
+    }
+  }
+
+  def find(id: Long): Option[Drink] = {
+    DB.withConnection {
+      implicit connection =>
+        SQL(
+          """
+            SELECT * FROM drink WHERE id = {id}
+          """
+        ).on(
+          'id -> id
+        ).as(aDrink.singleOpt)
+    }
+  }
+
+  def list: Seq[Drink] = {
+    DB.withConnection {
+      implicit connection =>
+        SQL(
+          """
+            SELECT * FROM drink
+          """
+        ).as(aDrink *)
+    }
+  }
+
+  def create(drink: Drink): Boolean = {
+    DB.withConnection {
+      implicit connection =>
+        SQL(
+          """
+            INSERT INTO drink (name, price, kind) VALUES ({name}, {price}, {kind})
+          """
+        ).on(
+          'name -> drink.name,
+          'price -> drink.price,
+          'kind -> drink.kind
+        ).executeUpdate() == 1
+    }
+  }
 
 }
